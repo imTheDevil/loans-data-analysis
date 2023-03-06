eea78bf39f3d16b97e6bc5e341dd8b5477eda7ab
imTheDevil <arr10@iitbbs.ac.in>
2023-03-06 12:04:18 +0530

    Changed a few queries

--- SQL Queries/Q3.sql
+++ SQL Queries/Q3.sql
@@ -1,12 +1,13 @@
 SELECT
 CASE
-WHEN tab.age<18 THEN 'Below 18'
-WHEN tab.age>=18 AND tab.age<30 THEN '18-29'
+WHEN tab.age<=18 THEN '18 or less'
+WHEN tab.age>=19 AND tab.age<30 THEN '19-29'
 WHEN tab.age>=30 AND tab.age<50 THEN '30-49'
 WHEN tab.age>=50 AND tab.age<70 THEN '50-69'
-ELSE 'Above 70'
+ELSE '70 or more'
 END as age_range,
 COUNT(*) as no_of_customers
 FROM
 (SELECT CAST(to_char(AGE(LOCALTIMESTAMP, "DOB"), 'YY') as INTEGER) as age FROM portfolio) as tab
-GROUP BY age_range;
+GROUP BY age_range
+ORDER BY min(tab.age);
--- SQL Queries/Q8.sql
+++ SQL Queries/Q8.sql
@@ -1,6 +1,9 @@
 -- This query assumes that the campaign atleast tried to contact the customer
 -- i.e., both FAILED and completed statuses.
-SELECT "Loan Number" as loan_num, COUNT(*) as no_of_calls FROM communication
+SELECT tab.no_of_calls, COUNT(*) as num_of_customers
+FROM
+(SELECT "Loan Number" as loan_num, COUNT(*) as no_of_calls FROM communication
 GROUP BY loan_num
-HAVING COUNT(*)<=5
-ORDER BY no_of_calls;
+HAVING COUNT(*)<=5) as tab
+GROUP BY tab.no_of_calls
+ORDER BY tab.no_of_calls;
